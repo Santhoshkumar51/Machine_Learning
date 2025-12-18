9d5779b7f561009a4f61b85087d9d9c3ca069867
Santhoshkumar51 <santhosh51429@gmail.com>
2025-12-18 21:54:46 +0530

    Machine Learning

--- App_MultipleLinearRegression.py
+++ App_MultipleLinearRegression.py
@@ -16,7 +16,7 @@ def load_css(file_path):
     with open(file_path) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
-load_css("styles.css")
+load_css("styles1.css")
 
 # Header
 st.markdown("""
